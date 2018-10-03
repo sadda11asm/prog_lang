22a0591c23603dd3aaa4e644bb66ecfca94c1426
Your Name <you@example.com>
2018-10-03 10:54:08 +0600

    lab 6

--- 5th/nr06.cpp
+++ 5th/nr06.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <map>
+#include <unordered_map>
 
 #include "listtest.h"
 #include "vectortest.h"
@@ -47,6 +49,91 @@ void list_sort() {
 }
 
 
+struct case_insensitive_hash {
+	size_t operator ( ) ( const std::string& s ) const {
+		size_t hash = 0;
+		for (size_t i = 0; i<s.length();i++) {
+			char c = std::tolower(s[i]);
+			hash+=i*(c-'a'+1);
+		}	
+		return hash;
+	}
+};
+
+struct case_insensitive_equality {
+	bool operator ( ) ( const std::string& s1, const std::string& s2 ) const {
+		if (s1.length() != s2.length()) return false;
+		size_t smallest = (s1.length()< s2.length()) ? s1.length() : s2.length();
+		for (size_t i = 0; i < smallest;i++) {
+			char c1 = std::tolower(s1[i]);
+			char c2 = std::tolower(s2[i]);
+			if (c1!=c2) return false;
+		}	
+		return true;
+	}
+};
+struct case_insensitive_cmp {
+	bool operator( ) ( const std::string& s1, const std::string& s2 ) const {
+		size_t smallest = (s1.length()< s2.length()) ? s1.length() : s2.length();
+		for (size_t i = 0; i < smallest;i++) {
+			char c1 = std::tolower(s1[i]);
+			char c2 = std::tolower(s2[i]);
+			if (c1<c2) return true;
+			if (c1>c2) return false;		
+		}	
+		return s1.length()<s2.length();
+	}
+	// Return true if s1 < s2, ignoring case of the letters.
+	
+};
+
+
+std::unordered_map< std::string, unsigned int,case_insensitive_hash, case_insensitive_equality > hashed_frequencytable( const std::vector< std::string > & text ) {
+	std::unordered_map< std::string, unsigned int, case_insensitive_hash, case_insensitive_equality> m;
+	for (size_t i=0; i<text.size(); i++) {
+		//if (m.find(text[i])==m.end()) m[text[i]] = 0;
+		m[text[i]]++;
+	}
+	return m;
+}
+std::map< std::string, unsigned int, case_insensitive_cmp > frequencytable( const std::vector< std::string > & text ) {
+	std::map< std::string, unsigned int, case_insensitive_cmp> m;
+	for (size_t i=0; i<text.size(); i++) {
+		//if (m.find(text[i])==m.end()) m[text[i]] = 0;
+		m[text[i]]++;
+	}
+	return m;
+}
+
+std::ostream& operator << ( std::ostream& stream, 
+const std::map< std::string, unsigned int, case_insensitive_cmp > & freq ) {
+	for (auto & x: freq) {
+		stream<<x.first<<" "<<x.second<<std::endl;	
+	}
+	return stream;
+}
+
+std::ostream& operator << ( std::ostream& stream, 
+const std::unordered_map< std::string, unsigned int,case_insensitive_hash, case_insensitive_equality > & freq) {
+	for (auto & x: freq) {
+		stream<<x.first<<" "<<x.second<<std::endl;	
+	}
+	return stream;
+}
+
+std::string mostfrequent (std::map< std::string, unsigned int, case_insensitive_cmp > m) {
+	unsigned int maxf = 0;
+	std::string val;	
+	for (auto & x: m) {
+		if (x.second>maxf) {
+			val = x.first;
+			maxf = x.second;
+		}
+	}
+	std::cout<<"\nfrequency: "<<maxf<<std::endl;
+	return val;
+} 
+
 
 int main( int argc, char* argv [] )
 {
@@ -57,12 +144,36 @@ int main( int argc, char* argv [] )
 	std::ifstream inp{"input.txt"};
 	vect = vectortest::readfile(inp);
 
-   	std::cout << vect << "\n";
+   	//std::cout << vect << "\n";
 
-   // Or use timer:
+	//std::vector<std::string > check = { "A", "a", "th", "AA", "aA", "Aa", "this", "THIS" };
+	std::map<std::string, unsigned int, case_insensitive_cmp> frequency = frequencytable(vect);
 
-	vector_sort();
-	list_sort();
+	std::cout << frequency;
+
+
+	std::cout << "magnus : " << frequency["magnus"] << "\n";
+
+	std::cout << "hominum : " << frequency["hominum"] << "\n";
+
+	std::cout << "memoria : " << frequency["memoria"] << "\n";
+
+	std::cout << "et : " << frequency["et"] << "\n";
+
+	std::cout<< "Most frequent " << mostfrequent(frequency) << std::endl;
+
+	
+	case_insensitive_cmp c;
+	std::cout << c( "a", "A" ) << c( "a","b" ) << c( "A", "b" ) << "\n";
+
+	case_insensitive_hash h;
+	std::cout << h( "xxx" ) << " " << h( "XXX" ) << "\n";
+	std::cout << h( "Abc" ) << " " << h( "abC" ) << "\n";
+
+	case_insensitive_equality e;
+	std::cout << e( "xxx", "XXXX" ) << "\n";
+	//vector_sort();
+	//list_sort();
 
    return 0;
 }
